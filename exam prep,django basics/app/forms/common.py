ee56cd5bfaf567495647f939ce8f96b22e343ad6
Niki Velchev <75383981+NikiVe@users.noreply.github.com>
2020-12-10 22:42:47 +0200

    Update with exam prep project
    Exam prep project,web basics with django,Soft Uni

---
+++ exam prep,django basics/app/forms/common.py
@@ -0,0 +1,7 @@
+class DisabledFormMixin:
+
+    def __init__(self):
+
+        for (_, field) in self.fields.items():
+            field.widget.attrs['disabled'] = True
+            field.widget.attrs['readonly'] = True
